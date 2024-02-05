fe6e808114b8944d6ec741662d6f880154abe659
amirjbr <153921915+amirjbr@users.noreply.github.com>
2024-02-05 16:16:49 +0330

    Combine Create and Update to Upsert

--- BulkyWeb/Controllers/ProductController.cs
+++ BulkyWeb/Controllers/ProductController.cs
@@ -21,7 +21,7 @@ namespace BulkyWeb.Controllers
             
             return View(objProductList);
         }
-        public IActionResult Create()
+        public IActionResult Upsert(int? id)
         {
             ProductVM productVM = new()
             {
@@ -34,10 +34,20 @@ namespace BulkyWeb.Controllers
                 Product = new Product()
                 
             };
-            return View(productVM);
+            if (id == null || id == 0)
+            {
+                return View(productVM);
+            }
+            else
+            {
+                //update
+                productVM.Product = _unitOfWork.Product.Get(u => u.ProductId == id);
+                return View(productVM);
+                
+            }
         }
         [HttpPost]
-        public IActionResult Create(ProductVM productVM)
+        public IActionResult Upsert(ProductVM productVM,IFormFile? file)
         {
             if (ModelState.IsValid)
             {
@@ -56,33 +66,8 @@ namespace BulkyWeb.Controllers
                 });
                 return View(productVM);
             }
-            
-
         }
-        public IActionResult Edit(int? id)
-        {
-            if (id == null || id == 0)
-            {
-                return NotFound();
-            }
-            Product? productFromDb = _unitOfWork.Product.Get(u => u.ProductId == id);
-            if (productFromDb == null)
-            {
-                return NotFound();
-            }
-            return View(productFromDb);
-        }
-        [HttpPost]
-        public IActionResult Edit(Product obj)
-        {
-            if (ModelState.IsValid)
-            {
-                _unitOfWork.Product.Update(obj);
-                _unitOfWork.Save();
-                return RedirectToAction("Index");
-            }
-            return View();
-        }
+        
         public IActionResult Delete(int? id)
         {
             if (id == null || id == 0)
